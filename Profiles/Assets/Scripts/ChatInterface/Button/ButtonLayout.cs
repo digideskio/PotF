793c17a539a86445a7283e512e5ba3ab4a4b1916
Pablo Leon-Luna <paleonluna@gmail.com>
2014-09-15 03:49:13 -0700

    All the great updates

--- Profiles/Assets/Scripts/ChatInterface/Button/ButtonLayout.cs
+++ Profiles/Assets/Scripts/ChatInterface/Button/ButtonLayout.cs
@@ -5,30 +5,30 @@ using System.Collections.Generic;
 
 public class ButtonLayout : MonoBehaviour {
 
-	public List<GameObject> ButtonList; //list of buttons as established in inspector/prefab which hold text values/receive input
+	public List<Button> buttonList; //list of buttons as established in inspector/prefab which hold text values/receive input
 	public float buttonVerticalDistance, buttonHorizontalDistance; //spacing between buttons
 	float topLeftX, topLeftY; //marks position of buttonlayout
 
 	void Awake ()
 	{
-		topLeftX = ButtonList [0].GetComponent<RectTransform> ().anchoredPosition.x; //sets up the button layout with respect to first button for designability
-		topLeftY = ButtonList [0].GetComponent<RectTransform> ().anchoredPosition.y;
+		DeactivateAll ();
+		topLeftX = buttonList [0].GetComponent<RectTransform> ().anchoredPosition.x; //sets up the button layout with respect to first button for designability
+		topLeftY = buttonList [0].GetComponent<RectTransform> ().anchoredPosition.y;
 	}
-
+	
 	public void Activate(List<string> wordChoices) //where wordChoices from dialogue asset
 	{
 		for (int i = 0; i < wordChoices.Count; i++)
 		{
-			RectTransform tempButton = ButtonList[i].GetComponent<RectTransform> ();
-			tempButton.anchoredPosition = new Vector2 (topLeftX + (i % 4) * (buttonHorizontalDistance), topLeftY + ((Mathf.FloorToInt(i / 4)) * buttonVerticalDistance)); //places buttons relative to canvas
-			tempButton.GetComponent<Text>().text = wordChoices[i]; //sets button text equal to text from Dialogue asset
-			ButtonList[i].SetActive(true); //makes button visible                                                                             										
+			buttonList[i].transform.localPosition = new Vector3 (topLeftX + (i % 4) * (buttonHorizontalDistance), topLeftY + ((Mathf.FloorToInt(i / 4)) * buttonVerticalDistance), 0.0f); //places buttons relative to canvas
+			buttonList[i].GetComponentInChildren<Text>().text = wordChoices[i]; //sets button text equal to text from Dialogue asset
+			buttonList[i].interactable = true; //makes button visible/usable                                                                             										
 		}
 	}
 
 	public void DeactivateAll()
 	{
-		foreach (GameObject button in ButtonList)
-			button.SetActive (false);
+		foreach (Button button in buttonList)
+			button.interactable = false; //deactivates all buttons
 	}
 }
--- Profiles/Assets/Scripts/ChatInterface/ChatPlayer.cs
+++ Profiles/Assets/Scripts/ChatInterface/ChatPlayer.cs
@@ -1,24 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class ChatPlayer : MonoBehaviour { //handles timer and delays + progress through texts
 
-	public Dialogue dialogueFile;
-	int progressCounter = 0;
-	public int delayRangeMin, delayRangeMax, userResponseDelay;
-	DialogueDisplayer dialogueDisplayer;
-	Characters currentCharacter;
-
-	//this should be rewritten such that there is more flexibility in terms of progress
+	public Dialogue dialogueFile; //holds all of the text and character information for a conversation
+	int progressCounter = 0; //keeps track of how far along we are in the conversation
+	public int delayRangeMin, delayRangeMax, userResponseDelay; //sets time in seconds of rhythm of conversation
+	Characters currentCharacter; //character enum used to identify whom with the user is talking and how to print the text
+	string submittedWords = ""; //a place holder for the sentences that the user is creating
+	public Text submissionSentence; //text that displays submittedWords
+	public Text[] displayedSentences = new Text[6]; //UI objects holding the sentences
+	GUIStyle style = new GUIStyle(); //allows text to be displayed with HTML formatting
+	List<string> dialogueTexts = new List<string>(); //holds the strings that 
+	public List<GameObject> buttonLayout;
+	public Button submit, reset;
 
 	void Start () {
-		StepsisterCycle ();
-		dialogueDisplayer = gameObject.GetComponent<DialogueDisplayer> ();
 		currentCharacter = dialogueFile.DialogItems [0].character; //sets conversation with character listed in the first element of dialogue
+		style.richText = true; //allows html modifications to text
+		StepsisterCycle (); //kicks off the conversation
 	}
 
-	void NextDialogueItem ()
+	void NextDialogueItem () //progresses forward to the next element in the Dialogue
 	{
 		if (progressCounter < dialogueFile.DialogItems.Count)
 		{
@@ -34,7 +39,7 @@ public class ChatPlayer : MonoBehaviour { //handles timer and delays + progress 
 	void StepsisterCycle() //sets the chat in motion, goes through sister first and then user/Jason
 	{
 		if (dialogueFile.DialogItems [progressCounter].characterSentence != null)
-			StartCoroutine ("sisterDelay");
+			StartCoroutine ("SisterDelay");
 		else
 			UserCycle ();
 	}
@@ -42,7 +47,7 @@ public class ChatPlayer : MonoBehaviour { //handles timer and delays + progress 
 	void UserCycle()
 	{
 		if (dialogueFile.DialogItems[progressCounter].wordChoices != null)
-			StartCoroutine ("userDelay");
+			StartCoroutine ("UserDelay");
 		else
 			NextDialogueItem();
 	}
@@ -52,6 +57,7 @@ public class ChatPlayer : MonoBehaviour { //handles timer and delays + progress 
 		//Pause for a random amount of time and then display sister's sentence
 		int delayInt = Random.Range (delayRangeMin, delayRangeMax);
 		yield return new WaitForSeconds (delayInt);
+		DisplayCharacterSentence (dialogueFile.DialogItems [progressCounter].characterSentence, currentCharacter);
 		UserCycle ();
 	}
 	
@@ -59,16 +65,100 @@ public class ChatPlayer : MonoBehaviour { //handles timer and delays + progress 
 	IEnumerator UserDelay()
 	{
 		yield return new WaitForSeconds (userResponseDelay);
-		progressCounter++;
-		//display the new list of buttons, wait til proper submission, get next item
-		
-		NextDialogueItem ();
+		//activate the button layout
+		GetComponent<ButtonLayout> ().Activate (dialogueFile.DialogItems [progressCounter].wordChoices);
+
 	}
 
+	//close the chat sequence
 	void EndChatSequence () {;}
-
-	public int ReturnProgCounter()
+	
+	public void DisplayCharacterSentence(string characterInput, Characters character)
 	{
-		return progressCounter;
+		switch (character) //depending on which character the string belongs to, displays a different style of text
+		{
+		case Characters.User : AddToDialogue ("<color=#6FFF47> Jason: </color>" + characterInput); break;
+		case Characters.Stepsister : AddToDialogue ( "<color=#CB2EFF> Stepsister: </color>" + characterInput); break;
+		case Characters.Pink : AddToDialogue ( "<color=#CB2EFF> Pink: </color>" + characterInput); break;
+		}
 	}
+	
+	void AddToDialogue(string sentenceText) //places sentences of a conversations in chronilogical order
+	{
+		if (dialogueTexts.Count < 6)
+		{
+			dialogueTexts.Add(sentenceText); //adds new dialogue to chat area while it still isnt full
+		}
+		
+		else
+			
+		{
+			for (int i = 1; i < 6; i++)
+			{
+				dialogueTexts[i-1] = dialogueTexts[i];
+			}
+			dialogueTexts[5] = sentenceText; //moves all text one slide down when it is full
+		}
+	}
+	
+	void Update()
+	{
+		if (dialogueTexts.Count > 0) //if either the user or another character has submitted a sentence
+		{
+			for (int i = 0; i < dialogueTexts.Count; i++) // 
+			{
+				displayedSentences[i].text = dialogueTexts[i];
+				displayedSentences[i].rectTransform.anchoredPosition = new Vector2 (0.1f, (1.0f - (float)i * 0.1f));
+			}
+		}	
+	}
+	
+	//The Following handles the accumulated sentence
+	
+	public void AddWord(string word)
+	{
+		submittedWords += word;
+		submissionSentence.text = submittedWords;
+		//change reset state to on
+		reset.interactable = true;
+		if (IsProperSentence (submittedWords))
+			//change submit state to active
+			submit.interactable = true;
+		else 
+			//change submit state to inactive
+			submit.interactable = false;
+
+	}
+
+	public void Submit()
+	{
+		DisplayCharacterSentence (submittedWords, Characters.User); //put sentence in display area
+		submittedWords = "";
+		//set submit to inactive
+		submit.interactable = false;
+	}
+
+	public void Reset()
+	{
+		submittedWords = "";
+		submissionSentence.text = submittedWords;
+		if (submittedWords != "") 
+			GetComponent<ButtonLayout> ().Activate (dialogueFile.DialogItems [progressCounter].wordChoices);
+		//change reset state inactive
+		reset.interactable = false;
+
+	}
+	
+	public bool IsProperSentence(string userAnswer) //checks to see if user has constructed valid sentence
+	{
+		for (int i = 0; i < dialogueFile.DialogItems[progressCounter].correctAnswers.Count; i++)
+		{
+			if (userAnswer == dialogueFile.DialogItems[progressCounter].correctAnswers[i])
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
