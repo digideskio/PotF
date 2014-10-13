b85dab6fb29b5692aa9e9b3ca81f73bd55eda309
Pablo Leon-Luna <paleonluna@gmail.com>
2014-10-13 13:35:19 -0400

    everything added in

---
+++ Profiles/Assets/Scripts/DreamSymbols/Chap1Condition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chap1Condition : MonoBehaviour {
+
+	public Animator leftOar, rightOar;
+	
+	// Update is called once per frame
+	void Update () {
+		if (leftOar.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Final")
+		    &&
+		    rightOar.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Final"))
+			StartCoroutine ("FadeOutOars");
+	}
+
+	IEnumerator FadeOutOars() {
+		print ("Fade Out");
+		yield return new WaitForSeconds (3);
+		leftOar.GetComponent<Animator> ().SetTrigger ("End");
+		rightOar.GetComponent<Animator> ().SetTrigger ("End");
+	}
+}
