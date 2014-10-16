bef77f7f1e3ab7d8408189aef45117efd80c7898
Pablo Leon-Luna <paleonluna@gmail.com>
2014-10-16 09:07:09 -0400

    fixed stupid IDE chat errors and fixed camera proportions on the chatinterface

--- Profiles/Assets/Scripts/ChatInterface/Button/ButtonLayout.cs
+++ Profiles/Assets/Scripts/ChatInterface/Button/ButtonLayout.cs
@@ -5,23 +5,23 @@ using System.Collections.Generic;
 
 public class ButtonLayout : MonoBehaviour {
 
-	public List<Button> buttonList; //list of buttons as established in inspector
+	public List<GameObject> buttonList; //list of buttons as established in inspector
 	public float buttonVerticalDistance, buttonHorizontalDistance; //spacing between buttons
 	float topLeftX, topLeftY; //marks position of buttonlayout
 
 	void Awake ()
 	{
 		DeactivateAll ();
-		topLeftX = buttonList [0].GetComponent<RectTransform> ().anchoredPosition.x; //sets up the button layout with respect to first button for designability
-		topLeftY = buttonList [0].GetComponent<RectTransform> ().anchoredPosition.y;
+		topLeftX = -450;
+		topLeftY = -165;
 	}
 	
 	public void Activate(List<string> wordChoices) //where wordChoices from dialogue asset
 	{
 		for (int i = 0; i < wordChoices.Count; i++) //wordChoices.Count
 		{
-			print ("activate");
-			buttonList[i].transform.localPosition = new Vector3 (topLeftX + (i % 4) * (buttonHorizontalDistance), topLeftY + ((Mathf.FloorToInt(i / 4)) * buttonVerticalDistance), 0.0f); //places buttons relative to canvas
+//			Vector3 ButtonPosition = new Vector3 (topLeftX + (i % 4) * (buttonHorizontalDistance), topLeftY + ((Mathf.FloorToInt(i / 4)) * buttonVerticalDistance), 0.0f); //places buttons relative to canvas
+//			buttonList[i].transform.position = ButtonPosition; //stupid error ruined this code so fuck it all
 			buttonList[i].gameObject.SetActive(true); //enables text
 			buttonList[i].GetComponentInChildren<Text>().text = wordChoices[i]; //sets button text equal to text from Dialogue asset
 		}
@@ -29,8 +29,8 @@ public class ButtonLayout : MonoBehaviour {
 
 	public void DeactivateAll()
 	{
-		foreach (Button button in buttonList) {
-			button.gameObject.SetActive(false); //disables text
+		foreach (GameObject button in buttonList) {
+			button.SetActive(false); //disables text
 		}
 	}
 }
