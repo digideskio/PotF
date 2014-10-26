07a93026c0ea2de421bb211ae30c4a1f40f11ea4
Pablo Leon-Luna <paleonluna@gmail.com>
2014-10-26 12:57:52 -0400

    pushing deletions

---
+++ Profiles/Assets/Scripts/Main Menu/MenuButton.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public enum Dimension {Present, Past, Future};
+
+public class MenuButton : MonoBehaviour {
+
+	public Dimension dimension;
+	public Animator image, buttonText;
+	public Image imageGO;
+	public AudioSource onHover, onClick;
+	public Animator presentText, pastText, futureText;
+
+	void OnMouseEnter()
+	{
+		image.SetTrigger ("Enter");
+		image.ResetTrigger ("Exit");
+
+		switch (dimension)
+		{
+		case Dimension.Future : futureText.SetTrigger ("Enter"); futureText.ResetTrigger("Exit"); break;
+		case Dimension.Past : pastText.SetTrigger ("Enter"); pastText.ResetTrigger("Exit"); break;
+		case Dimension.Present : presentText.SetTrigger ("Enter"); presentText.ResetTrigger("Exit"); break;
+		}
+	}
+	
+	void OnMouseExit()
+	{
+		image.SetTrigger ("Exit");
+		image.ResetTrigger ("Enter");
+		switch (dimension)
+		{
+		case Dimension.Future : futureText.SetTrigger ("Exit"); futureText.ResetTrigger("Enter"); break;
+		case Dimension.Past : pastText.SetTrigger ("Exit"); pastText.ResetTrigger("Enter"); break;
+		case Dimension.Present : presentText.SetTrigger ("Exit"); presentText.ResetTrigger("Enter"); break;
+		}
+	}
+	
+	void OnMouseDown()
+	{
+		if (image.GetCurrentAnimatorStateInfo(0).IsName("Hover"))
+		{
+			image.SetTrigger ("Click");
+			onClick.Play ();
+		}
+	}
+}
