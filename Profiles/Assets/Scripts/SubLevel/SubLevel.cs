fa8aac025e3dfd6bf982b781b1c414d68a6803f7
Pablo Leon-Luna <paleonluna@gmail.com>
2014-11-10 15:55:30 -0500

    a chunk

--- Profiles/Assets/Scripts/SubLevel/SubLevel.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class SubLevel : ScriptableObject {
-
-	public List<SubLevelElement> SubLevelItems; //in a chapter, one list or asset is used to store the different stages of that chapter, of which there are at most 2 or 3.
-
-}
--- Profiles/Assets/Scripts/SubLevel/SubLevelElement.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-
-[System.Serializable]
-public class SubLevelElement
-{
-	public string Autosymbolism, Maze, Flashback, Chat; //the names of the buttons on each variation on a sublevel
-}
