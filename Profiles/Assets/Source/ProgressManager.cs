76a39c89dcaeeaee054cd0c0936c9f2148cc889b
Pablo Leon-Luna <paleonluna@gmail.com>
2014-11-11 10:06:47 -0500

    got sub levels figured out

--- Profiles/Assets/Source/ProgressManager.cs
+++ Profiles/Assets/Source/ProgressManager.cs
@@ -6,10 +6,10 @@ public class ProgressManager : MonoBehaviour {
 
 	public static int level = 1;
 	public static int subLevel = 1;
-	public static bool AutoSymbolism = false;
-	public static bool FlashBack = false;
-	public static bool Maze = false;
-	public static bool Chat = false;
+	public static bool AutoSymbolism = true;
+	public static bool FlashBack = true;
+	public static bool Maze = true;
+	public static bool Chat = true;
 
 
 	public static void IncrementLevel()
@@ -21,10 +21,20 @@ public class ProgressManager : MonoBehaviour {
 	public static void IncrementSubLevel()
 	{
 		subLevel ++;
+		MakeChallengesTrue ();
 	}
 
 	public static void ResetSubLevel ()
 	{
 		subLevel = 1;
+		MakeChallengesTrue ();
+	}
+
+	static void MakeChallengesTrue ()
+	{
+		AutoSymbolism = true;
+		FlashBack = true;
+		Maze = true;
+		Chat = true;
 	}
 }
--- Profiles/Assets/Source/SubLevel/SubLevelInitializer.cs
+++ Profiles/Assets/Source/SubLevel/SubLevelInitializer.cs
@@ -1,13 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SubLevelInitializer : MonoBehaviour {
 
 	public GameObject mazeButton, autosymbolismButton, chatButton, flashbackButton;
-
+	public SubLevel subLevelData;
 	// looks at ProgressManager to initialize buttons which lead to other level using their string values which are called from sublevelassets
-	void Start () {
-	
-	}
+	void Start ()
+	{
+		if (ProgressManager.AutoSymbolism == true && subLevelData.SubLevelItems[ProgressManager.subLevel].Autosymbolism != "")
+		{
+			autosymbolismButton.GetComponentInChildren<Text>().text = subLevelData.SubLevelItems[ProgressManager.subLevel].Autosymbolism;
+			autosymbolismButton.SetActive(true);
+		}
 
+		if (ProgressManager.Maze == true && subLevelData.SubLevelItems[ProgressManager.subLevel].Maze != "")
+		{
+			mazeButton.GetComponentInChildren<Text>().text = subLevelData.SubLevelItems[ProgressManager.subLevel].Maze;
+			mazeButton.SetActive(true);
+		}
+
+		if (ProgressManager.Chat == true && subLevelData.SubLevelItems[ProgressManager.subLevel].Chat != "")
+		{
+			chatButton.GetComponentInChildren<Text>().text = subLevelData.SubLevelItems[ProgressManager.subLevel].Chat;
+			chatButton.SetActive(true);
+		}
+
+		if (ProgressManager.FlashBack == true && subLevelData.SubLevelItems[ProgressManager.subLevel].Flashback != "")
+		{
+			flashbackButton.GetComponentInChildren<Text>().text = subLevelData.SubLevelItems[ProgressManager.subLevel].Flashback;
+			flashbackButton.SetActive(true);
+		}
+	}
 }
