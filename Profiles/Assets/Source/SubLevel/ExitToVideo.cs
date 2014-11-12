e7b0675670b736dadbc0032b4fa2464b59397325
Pablo Leon-Luna <paleonluna@gmail.com>
2014-11-12 12:46:07 -0500

    getting the flow down, added hidden text

---
+++ Profiles/Assets/Source/SubLevel/ExitToVideo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitToVideo : MonoBehaviour {
+
+	public int subLevelValue;
+
+	public GameObject challenge1, challenge2, challenge3;
+
+	void Update ()
+	{
+		if (challenge1.activeSelf == false && challenge2.activeSelf == false && challenge3.activeSelf == false && ProgressManager.subLevel == subLevelValue)
+			gameObject.SetActive (true);
+	}
+
+}
---
+++ Profiles/Assets/Source/SubLevel/HiddenText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HiddenText : MonoBehaviour {
+
+	public Animator image, buttonText;
+	public AudioSource onHover;
+	
+	void Awake ()
+	{
+		Image buttonPNG = GetComponentInParent<Image> ();
+	}
+	
+	void OnMouseEnter()
+	{
+		onHover.Play ();
+		image.SetTrigger ("Enter");
+		image.ResetTrigger ("Exit");
+	}
+	
+	void OnMouseExit()
+	{
+		image.SetTrigger ("Exit");
+		image.ResetTrigger ("Enter");
+	}
+}
