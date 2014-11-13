16189621c1e4730df06410e9298aeabb98b3abc6
Pablo Leon-Luna <paleonluna@gmail.com>
2014-11-13 10:05:59 -0500

    maze basically fixed

--- Profiles/Assets/Source/Mazes/RandomDrift.cs
+++ Profiles/Assets/Source/Mazes/RandomDrift.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class RandomDrift : MonoBehaviour {
 
-	float m_TimeStamp;
 	public float driftSpeed, randomRange;
 	bool flowGate = true;
 
@@ -11,7 +10,6 @@ public class RandomDrift : MonoBehaviour {
 	{
 		if (flowGate)
 		{
-			transform.rotation = Quaternion.Euler(0, transform.rotation.x, transform.rotation.y);
 			StartCoroutine ("Shake");
 			flowGate = false;
 
@@ -20,15 +18,25 @@ public class RandomDrift : MonoBehaviour {
 
 	IEnumerator Shake ()
 	{
+		/*
+		The problem with our shake is that it is randomly drifting such that the character over a long time is facing the wrong direction.
+		To remedy this I will first try using rotate fnc. If that does not work, I will try to conceive of a more complex system that ensures to not move the character more than 
+		a certain amount of degrees in one direction without going back in the other direction, we could do this my 
+
+		*/
+
 		float randX = Random.Range(-randomRange, randomRange);
 		float randZ = Random.Range(-randomRange, randomRange);
-		Quaternion currentRot = transform.rotation;
-		Quaternion targetRot = Quaternion.Euler(currentRot.x, currentRot.y + randX, currentRot.z + randZ);
-		transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * (1.0f/driftSpeed));
-		yield return new WaitForSeconds(0.01f);
-		Quaternion rebound = Quaternion.Euler(0, transform.rotation.y + randX, 0);
-		transform.rotation = Quaternion.Slerp(transform.rotation, rebound, Time.deltaTime * (1.0f/driftSpeed));
-		yield return new WaitForSeconds(1.0f/driftSpeed);
+//		Quaternion currentRot = transform.rotation;
+//		Quaternion targetRot = Quaternion.Euler(currentRot.x, currentRot.y + randX, currentRot.z + randZ);
+//		transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * (1.0f/driftSpeed));
+//		yield return new WaitForSeconds(0.01f);
+//		Quaternion rebound = Quaternion.Euler(0, transform.rotation.y + randX, 0);
+//		transform.rotation = Quaternion.Slerp(transform.rotation, rebound, Time.deltaTime * (1.0f/driftSpeed));
+//		yield return new WaitForSeconds(1.0f/driftSpeed);
+		transform.Rotate(Vector3.down, randX);
+		transform.Rotate(Vector3.left, randZ);
+		yield return new WaitForSeconds (0.01f);
 
 		flowGate = true;
 	}
