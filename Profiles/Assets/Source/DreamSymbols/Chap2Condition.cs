e9fdd2ab3c93aac120d1ecb880b5c990b7b4b0bf
Pablo Leon-Luna <paleonluna@gmail.com>
2014-11-14 12:32:35 -0500

    working on #2 autosym

---
+++ Profiles/Assets/Source/DreamSymbols/Chap2Condition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chap2Condition : MonoBehaviour {
+
+	public Animator jacket, camerax;
+	public GameObject pink1, pink2;
+	bool isPinkReadytoHoldHand;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0))
+		{
+			if (jacket.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Clickable"))
+			{
+				//set camera movement animation
+				camerax.SetTrigger("Pan");
+			}
+		}
+
+	}
+}
