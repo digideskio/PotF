aaff87a631a33bd3f4056bee4719afc4b4c340d0
Pablo Leon-Luna <paleonluna@gmail.com>
2014-11-27 22:58:54 -0800

    thxgiving prgoress

---
+++ Profiles/Assets/Source/DreamSymbols/BallTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallTrigger : MonoBehaviour {
+
+	public GameObject PinkShirtReachOut;
+	
+	// Update is called once per frame
+	void Update () {
+		if (PinkShirtReachOut.activeSelf == true)
+			gameObject.GetComponent<Animator> ().SetTrigger ("Fade");
+
+	}
+}
