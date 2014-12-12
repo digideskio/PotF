9f8bc79c1febcbb8dce9d50aea4631b0aa142093
Pablo Leon-Luna <paleonluna@gmail.com>
2014-12-12 10:13:23 -0800

    added striptease

---
+++ Profiles/Assets/Source/DreamSymbols/StripTease.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StripTease : MonoBehaviour {
+
+	public Image[] stripMode;
+	int i = 0;
+
+	void OnTriggerEnter() {
+		if (i < 7) {
+			stripMode[i].enabled = false;
+			i++;
+			stripMode[i].enabled = true;
+		}
+	}
+}
