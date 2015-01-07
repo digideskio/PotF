f2b44b53576ba8cf662ad6b4e8c8c199b38db882
Pablo Leon-Luna <paleonluna@gmail.com>
2015-01-06 21:56:03 -0800

    potf update

---
+++ Profiles/Assets/Source/DreamSymbols/OpenBackBack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenBackBack : MonoBehaviour {
+
+	GameObject bp1, bp2;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
