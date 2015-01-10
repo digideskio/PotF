2bc7a55b0292014c2ec553bba7692451757cae6b
Pablo Leon-Luna <paleonluna@gmail.com>
2015-01-10 12:33:50 -0800

    finish animation

--- Profiles/Assets/Source/DreamSymbols/OpenBackBack.cs
+++ Profiles/Assets/Source/DreamSymbols/OpenBackBack.cs
@@ -3,8 +3,23 @@ using System.Collections;
 
 public class OpenBackBack : MonoBehaviour {
 
-	GameObject bp1, bp2;
+	public GameObject bp1, bp2;
+	public Animator pink1;
+	bool isOpened = false;
 
+	void OnMouseDown () {
+		if (isOpened == false) {
+			bp1.SetActive (false);
+			bp2.SetActive (true);
+			isOpened = true;
+		}
+
+		else if (isOpened) {
+			pink1.SetTrigger("fade");
+			GameObject.Find ("LeftOar").GetComponent<SliceOar>().canSlice = true;
+			GameObject.Find ("RightOar").GetComponent<SliceOar>().canSlice = true;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
--- Profiles/Assets/Source/DreamSymbols/SliceOar.cs
+++ Profiles/Assets/Source/DreamSymbols/SliceOar.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 
 public class SliceOar : MonoBehaviour {
 
+	public static SliceOar s_instance;
+
 	public GameObject leftArm, rightArm;
 	public GameObject corpse;
-	public bool isRight;
+	public bool isRight, canSlice = false;
 	public Animator thisOar;
 	void Start() {
 		thisOar = GetComponent<Animator> ();
@@ -14,13 +16,12 @@ public class SliceOar : MonoBehaviour {
 
 	void OnMouseDown() {
 		thisOar.SetTrigger ("Slice");
-		if (!isRight) {
-			print ("pull right");
+		if (!isRight && canSlice) {
 			corpse.GetComponent<Dismemberment> ().DismemberRight ();
 			leftArm.SetActive(true);
 			leftArm.GetComponent<Animator>().SetTrigger("Slice");
 		}
-		else {
+		else if(isRight && canSlice) {
 			corpse.GetComponent<Dismemberment> ().DismemberLeft ();
 			rightArm.SetActive(true);
 			rightArm.GetComponent<Animator>().SetTrigger("Slice");
