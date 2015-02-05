ead54936450e50bf41170671943d824a106e1596
Pablo Leon-Luna <paleonluna@gmail.com>
2015-02-04 18:15:40 -0800

    workin on fixing flashbacks and shit

---
+++ Profiles/Assets/Holder.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Holder : MonoBehaviour {
+
+	public Image placeHolderSprite;
+	public Sprite temp;
+	// Use this for initialization
+	void Start () {
+		placeHolderSprite = GetComponent<Image> ();
+		temp = Resources.Load ("Art/MainMenu/ButtonSmall", typeof(Sprite)) as Sprite;
+		placeHolderSprite.sprite = temp;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
