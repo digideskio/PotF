76f6a75af2bad84cf53fb6a28940cad927dae7a5
Pablo Leon-Luna <paleonluna@gmail.com>
2015-03-22 20:37:36 -0700

    adding in sound to maze

--- Profiles/Assets/Source/Mazes/WinCondition.cs
+++ Profiles/Assets/Source/Mazes/WinCondition.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class WinCondition : MonoBehaviour {
 
+	public AudioSource soundtrack;
+
+	void Start(){
+		SoundtrackManager.s_instance.PlayAudioSource (SoundtrackManager.s_instance.lakeAmbient);
+		soundtrack.Play ();
+	}
+
 	void OnTriggerEnter() {
 		StartCoroutine ("Win");
 	}
@@ -15,7 +22,16 @@ public class WinCondition : MonoBehaviour {
 		GameObject.Find ("whiteOut").GetComponent<FadeIn>().StartFade();
 		yield return new WaitForSeconds (4f);
 		Time.timeScale = 1f;
+		SoundtrackManager.s_instance.StartCoroutine("FadeOutAudioSource",SoundtrackManager.s_instance.megaAmbient);
 		Application.LoadLevel (GameManager.s_instance.subLevel);
 
+	}
+
+	IEnumerator FadeOutAudioSource(AudioSource x) { //call from elsewhere
+		while (x.volume > 0.0f) {					//where x is sound track file
+			x.volume -= 0.1f;
+			yield return new WaitForSeconds(0.3f);
+		}
+		x.Stop ();
 	}
 }
