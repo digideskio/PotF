bc15d2efa9ae546feb35cfc02a641d180305c1b3
Pablo Leon-Luna <paleonluna@gmail.com>
2015-03-24 00:58:07 -0700

    killer

---
+++ Profiles/Assets/AudioTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTrigger : MonoBehaviour {
+
+
+	AudioSource audiobit;
+	// Use this for initialization
+	void Start () {
+		audiobit = GetComponent<AudioSource> ();
+	}
+	
+	void OnTriggerEnter() {
+		if (audiobit.isPlaying == false) {
+			audiobit.Play();
+		}
+	}
+}
