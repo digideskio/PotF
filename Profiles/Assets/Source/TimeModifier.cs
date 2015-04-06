16c156263e2ad0b97bef4358cdb1d17b1426d9bc
Pablo Leon-Luna <paleonluna@gmail.com>
2015-04-06 14:13:59 -0700

    all non video changes

---
+++ Profiles/Assets/Source/TimeModifier.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeModifier : MonoBehaviour {
+	public float StartTime;
+	// Use this for initialization
+	void Start () {
+		GetComponent<AudioSource> ().time = StartTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
